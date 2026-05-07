67d4dfd98da5d6cec2bee1472e0a649ec8dfcd1d
Elias <elias@falchus.com>
2026-05-08 00:25:51 +0200

    use mixerno estv3 api

--- assets/js/live.core.js
+++ assets/js/live.core.js
@@ -1,11 +1,11 @@
 YT.live = {
   channelID: "",
   update: function () {
-    $.getJSON("https://mixerno.space/api/youtube-channel-counter/user/" + this.channelID, function (e) {
+    $.getJSON("https://backend.mixerno.space/api/youtube/estv3/" + this.channelID, function (e) {
       if (e) {
-        YT.updateManager.updateSubscribers(e.counts[2].count);
-        YT.updateManager.updateViews(e.counts[3].count);
-        YT.updateManager.updateVideos(e.counts[5].count);
+        YT.updateManager.updateSubscribers(e.items[0].statistics.subscriberCount);
+        YT.updateManager.updateViews(e.items[0].statistics.viewCount);
+        YT.updateManager.updateVideos(e.items[0].statistics.videoCount);
       } else {
         YT.query.newSearch(YT.live.channelID);
       }
