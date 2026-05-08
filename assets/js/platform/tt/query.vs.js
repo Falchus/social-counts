4890fa74d92f6b65d6ff29784098ea25e9ba9925
Elias <elias@falchus.com>
2026-05-08 23:13:32 +0200

    fix tiktok

--- assets/js/platform/tt/query.vs.js
+++ assets/js/platform/tt/query.vs.js
@@ -6,8 +6,8 @@ TT.query = {
         $.getJSON(
           "//mixerno.space/api/tiktok-user-counter/user/" + encodeURIComponent(TT.live.vs2),
           function (g) {
-          TT.updateManager.updateName(f.user[0].count, g.user[0].count);
-          TT.updateManager.updateProfile(f.user[1].count, g.user[1].count);
+            TT.updateManager.updateName(f.user[0].count, g.user[0].count);
+            TT.updateManager.updateProfile(f.user[1].count, g.user[1].count);
           },
         );
       },
