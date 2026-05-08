2f10c2d54a4cf24791a0f2a59dd6904f4303da4d
Elias <elias@falchus.com>
2026-05-08 23:14:36 +0200

    remove sorting from social-counts compare

--- assets/js/platform/tt/urlManager.vs.js
+++ assets/js/platform/tt/urlManager.vs.js
@@ -1,26 +1,14 @@
 TT.urls = {
   onchange: function () {
     let q = location.hash.split("!/")[1];
-    let c;
     if (q) {
       q = q.split("$$");
-      if (q[0] > q[1]) {
-        c = q[0];
-        q[0] = q[1];
-        q[1] = c;
-      }
       TT.urls.pushState(q[0], q[1]);
     } else {
       TT.urls.pushState("khaby.lame", "charlidamelio");
     }
   },
   pushState: function (e, f) {
-    let c;
-    if (e > f) {
-      c = e;
-      e = f;
-      f = c;
-    }
     history.pushState(null, null, "#!/" + e + "$$" + f);
     TT.live.setVS(e, f);
   }
